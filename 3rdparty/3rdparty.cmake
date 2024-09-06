f9bab1d98917cfc8c4f2e8d32cae4ba971610df4
Zhe Lei <41496773+Sknp1006@users.noreply.github.com>
2024-09-06 19:00:22 +0800

    build: 新增依赖库 whereami

--- 3rdparty/3rdparty.cmake
+++ 3rdparty/3rdparty.cmake
@@ -1,3 +1,20 @@
 add_subdirectory(${3RD_PARTY_DIR}/eigen)
 add_subdirectory(${3RD_PARTY_DIR}/spdlog)
 add_subdirectory(${3RD_PARTY_DIR}/googletest)
+add_subdirectory(${3RD_PARTY_DIR}/whereami)
+
+#include the 3rdparty headers
+set(3RD_PARTY_INCLUDE_DIRS
+    ${3RD_PARTY_DIR}/eigen
+    ${3RD_PARTY_DIR}/spdlog/include
+    ${3RD_PARTY_DIR}/whereami
+    CACHE INTERNAL "3rdparty include directories"
+)
+
+#libs
+set(3RD_PARTY_LIBS
+    eigen
+    spdlog
+    whereami
+    CACHE INTERNAL "3rdparty libraries"
+)
